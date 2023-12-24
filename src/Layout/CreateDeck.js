41b763a8851659cdf067aab0d87454656fbc060d
Malik <Mata@indeed.com>
2023-12-24 15:59:01 -0800

    Update submit button to add new deck

--- src/Layout/CreateDeck.js
+++ src/Layout/CreateDeck.js
@@ -1,53 +1,73 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React, { useState } from "react";
+import { Button, Form } from 'react-bootstrap';
+import { createDeck } from "../utils/api/index"; // Adjust the path accordingly
+import { API_BASE_URL, stripCards, fetchJson } from "../utils/api/index"; // Import API-related functions
+
 import './CreateDeck.css';
-import { useState } from "react";
 
 function CreateDeck() {
-
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
 
-  const handleNameChange= (event) => {
-    setName(event.target.value)
-  }
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
 
-  const handleDescriptionChange= (event) => {
-    setDescription(event.target.value)
-  }
-  const handleOnSubmit= (event) => {
-    
-  }
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handleOnSubmit = async (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    try {
+      // Create a new deck object
+      const newDeck = {
+        name,
+        description,
+      };
+
+      // Call the createDeck function to create a new deck
+      await createDeck(newDeck);
+
+      // Optionally, you can add logic to handle success, navigate to another page, etc.
+
+      // Clear the form fields after submission
+      setName("");
+      setDescription("");
+    } catch (error) {
+      console.error("Error creating deck:", error);
+    }
+  };
+
   return (
     <div className="create-deck-container">
       <h2>Create Deck</h2>
-      <Form>
+      <Form onSubmit={handleOnSubmit}>
         <Form.Group controlId="formName">
           <Form.Label>Name</Form.Label>
-          <Form.Control 
-          type="text" 
-          placeholder="Enter name"
-          value={name}  // Set the value to the current state
-          onChange={handleNameChange}  // Handle changes
+          <Form.Control
+            type="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={handleNameChange}
           />
         </Form.Group>
         <Form.Group controlId="formDescription">
           <Form.Label>Description</Form.Label>
-          <Form.Control 
-          as="textarea" 
-          rows={3} 
-          placeholder="Enter description" 
-          value={description}  // Set the value to the current state
-          onChange={handleDescriptionChange}  // Handle changes
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder="Enter description"
+            value={description}
+            onChange={handleDescriptionChange}
           />
         </Form.Group>
         <div className="button-container">
           <Button variant="secondary" className="cancel-button">
             Cancel
           </Button>
-          <Button variant="primary" className="submit-button">
+          <Button type="submit" variant="primary" className="submit-button">
             Submit
           </Button>
         </div>
