e7ea55998da26ad974a9fed8c3c8663295e43df0
Malik <Mata@indeed.com>
2023-12-29 05:20:58 -0800

    Fix bug

--- src/Layout/Decks/Decks.js
+++ src/Layout/Decks/Decks.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { listDecks } from "../../utils/api/index"; // Adjust the path accordingly
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-
-import { deleteDeck } from "../../utils/api/index"; // Adjust the path accordingly
+import { listDecks } from "../../utils/api/index";
+import { deleteDeck } from "../../utils/api/index";
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import CreateDeckButton from "./CreateDeckButton";
